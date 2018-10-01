c3ebbe1711121c964581d5dad2a1b7525f4c00de
Amar97 <amarramdas97@gmail.com>
2018-10-02 01:18:03 +0200

    created a simple nav bar
    Signed-off-by: Amar97 <amarramdas97@gmail.com>

---
+++ src/Components/Menu.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import logo from '../logo.svg';
+
+class Menu extends Component {
+	render() {
+		return (
+			<div id="navbar" className="Menu">
+				<img class="logo" src={logo} alt="logo"/>
+				<a href="#home">Home</a>
+				<a href="#news">Pricing</a>
+				<a href="#contact">Contact</a>
+			</div>
+		);
+	}
+}
+
+export default Menu;
