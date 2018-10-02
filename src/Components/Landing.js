69aa4872a2cae1dbae2ebd0350f40e9ff1df8910
Amar97 <amarramdas97@gmail.com>
2018-10-02 15:00:39 +0200

    made some improvements to the navbar and added illustration for the landing page.
    Signed-off-by: Amar97 <amarramdas97@gmail.com>

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,21 +1,31 @@
 import React, {Component} from 'react';
 import lowpoly from '../images/lowpoly.jpg';
 import techy from '../images/techy.png';
+import browser from '../images/browserflat.png';
 
 var sectionStyle = {
 	width: "100%",
 	height: "650px",
-	backgroundImage: `url(${lowpoly})`
+	backgroundImage: `url(${lowpoly})`,
+	paddingTop: "0"
+};
+
+var landingStyle = {
+	float: "right",
+	width: "40%"
+
 };
 
 class Clients extends Component {
 	render() {
 		return (
-			<div className="ClientList">
-				<div className="customer-logos" style={sectionStyle}>
-
+			<div class="landing" style={sectionStyle} className="Landing">
+				<div class="landing-wrapper">
+					<h1>ASRR delivers expert problem analytics and solutions in web, desktop, app and many more
+						environments.</h1>
+					<img style={landingStyle} src={browser} alt="background"/>
 				</div>
-				<img src={techy}/>
+				<img src={techy} alt="background"/>
 			</div>
 		);
 	}
