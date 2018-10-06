92642dff4e563a6773da4b94bf1065f52794cfa3
Amar97 <amarramdas97@gmail.com>
2018-10-06 20:57:18 +0200

    fixed router
    Signed-off-by: Amar97 <amarramdas97@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Link, Route} from 'react-router-dom';
+
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {
 	faAndroid,
@@ -6,7 +8,7 @@ import {
 	faAppStoreIos,
 	faAws,
 	faChrome,
-	faCss3,
+	faCss3Alt,
 	faFacebook,
 	faGit,
 	faGithub,
@@ -23,26 +25,90 @@ import {
 	faTwitter,
 	faWindows
 } from '@fortawesome/free-brands-svg-icons'
-import Menu from './Components/Menu';
 import Landing from './Components/Landing';
 import Clients from './Components/Clients';
 import SimpleSlider from "./Components/Slider";
 import Services from "./Components/Services";
 import Footer from "./Components/Footer"
 import './App.css';
+import logo from "./logo.svg";
 
-library.add(faHtml5, faCss3, faJs, faJava, faWindows, faLinux, faGithub, faGit, faReact, faAndroid, faAppStoreIos, faApple, faNode, faNodeJs, faPaypal, faFacebook, faTwitter, faSnapchat, faAws, faChrome, faLinkedin);
+library.add(faHtml5, faCss3Alt, faJs, faJava, faWindows, faLinux, faGithub, faGit, faReact, faAndroid, faAppStoreIos, faApple, faNode, faNodeJs, faPaypal, faFacebook, faTwitter, faSnapchat, faAws, faChrome, faLinkedin);
+
+const Homepage = () => (
+	<div>
+		<Landing/>
+		<SimpleSlider/>
+		<Services/>
+		<Clients/>
+	</div>
+);
+
+const Portfolio = () => (
+	<div>
+		<Landing/>
+		<Landing/>
+		<Landing/>
+		<Landing/>
+	</div>
+);
+
+const About = () => (
+	<div>
+		<ul>
+			<li>San Francisco</li>
+			<li>Istanbul</li>
+			<li>Tokyo</li>
+		</ul>
+	</div>
+);
+
+const Careers = () => (
+	<div>
+		<ul>
+			<li>San Francisco</li>
+			<li>Istanbul</li>
+			<li>Tokyo</li>
+		</ul>
+	</div>
+);
+
+const Contact = () => (
+	<div>
+		<ul>
+			<li>San Francisco</li>
+			<li>Istanbul</li>
+			<li>Tokyo</li>
+		</ul>
+	</div>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-	      <Menu/>
-	      <Landing/>
-	      <SimpleSlider/>
-		  <Services/>
-	      <Clients/>
-		  <Footer/>
+	      <div>
+		      <header className="header">
+			      <Link to="/"><img className="logo" src={logo} alt="logo"/></Link>
+			      <input className="menu-btn" type="checkbox" id="menu-btn"/>
+			      <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"/></label>
+			      <ul className="menu">
+				      <li><Link to="/">Home</Link></li>
+				      <li><Link to="/portfolio">Portfolio</Link></li>
+				      <li><Link to="/about">About</Link></li>
+				      <li><Link to="/careers">Careers</Link></li>
+				      <li><Link to="/contact">Contact</Link></li>
+			      </ul>
+		      </header>
+	      </div>
+
+	      <Route path="/" exact component={Homepage}/>
+	      <Route path="/portfolio" component={Portfolio}/>
+	      <Route path="/about" component={About}/>
+	      <Route path="/careers" component={Careers}/>
+	      <Route path="/contact" component={Contact}/>
+
+	      <Footer/>
       </div>
     );
   }
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -6,13 +6,13 @@ class Clients extends Component {
 	render() {
 		return (
 			<div class="landing-wrapper">
-				<div id="one">
+				<div id="landing-text">
 					<h1>ASRR delivers expert problem analytics and solutions in web/app development, desktop
 						applications and many more
 						environments.</h1>
 				</div>
 
-				<div id="two">
+				<div id="landing-image">
 					<img src={browser} alt="browser"/>
 				</div>
 			</div>
