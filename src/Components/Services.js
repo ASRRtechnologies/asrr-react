aa75d0399116b700b5593ef1c4304cfc3ce6f3c3
Amar97 <amarramdas97@gmail.com>
2018-10-06 23:41:38 +0200

    made services component responsive
    Signed-off-by: Amar97 <amarramdas97@gmail.com>

--- src/Components/Services.js
+++ src/Components/Services.js
@@ -7,7 +7,7 @@ import phone from "../images/services/phone.svg";
 class Services extends Component {
     render() {
         return (
-            <div class="clients-wrapper">
+	        <div className="clients-wrapper">
                 <div>
                     <h1>We specialize in the following services</h1>
                 </div>
@@ -15,22 +15,22 @@ class Services extends Component {
                 <div className="flex-container">
                     <div>
 	                    <img src={laptop}/>
-                        <h1>Webdesign</h1>
+	                    <h2>Webdesign</h2>
                         <p>Responsive and beautiful websites, like the one you're on now. From the ground up, using html5, css, frameworks such as ReactJS, or choose one of our existing templates.</p>
                     </div>
                     <div>
 	                    <img src={phone}/>
-                        <h1>App Development</h1>
+	                    <h2>App Development</h2>
                         <p>We can develop both Android an IOS apps that have seamless integrations to your our our custom back-end.</p>
                     </div>
                     <div>
 	                    <img src={router}/>
-                        <h1>Custom back-end applications</h1>
+	                    <h2>Custom back-end applications</h2>
                         <p>The back-ends we deliver are Rest-ful, meaning that one backend can be used for your website, app and desktop application. We use Java, Spring, Spring-Boot and SQL.</p>
                     </div>
                     <div>
 	                    <img src={computer}/>
-                        <h1>Windows Desktop Applications</h1>
+	                    <h2>Windows Desktop Applications</h2>
                         <p>We can create beautiful custom desktop applications using C#, to automate and speed up complex operations.</p>
                     </div>
                 </div>
