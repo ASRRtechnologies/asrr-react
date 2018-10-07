dd051ff97b8873ee4eda7616916bcf0ac10a1c86
Amar Ramdas <amarramdas97@gmail.com>
2018-10-07 23:36:52 +0200

    Add animation to portfolio page and fix css

--- src/components/portfolio/Websites.js
+++ src/components/portfolio/Websites.js
@@ -1,37 +1,37 @@
 import React, {Component} from 'react';
 import kroontje from '../../images/portfolio/kroontje.png';
-import lowpolypurple from '../../images/lowpolypurple.svg';
-
-var lowPolyBackground = {
-    backgroundImage: lowpolypurple
-}
+import Fade from 'react-reveal/Fade';
 
 class Websites extends Component {
     render() {
         return (
             <div className="websites">
                 <h1>Web</h1>
-                <div className="background-wrapper">
-                    <div className="portfolio-container" style={lowPolyBackground}>
-                        <div>
-                            <img src={kroontje}/>
-                            <h2>Eethuys 't Kroontje</h2>
-                            <p>The website for Eethuys 't Kroontje was one of the first websites we actually built. It was made using a very simple and responsive design. If you're looking for good food in a relaxing environment, check out their site by clicking the image above. If you're curious to a super secret prototype we made, click this.</p>
-                        </div>
+                <Fade bottom>
+                    <div className="background-wrapper">
+                        <div className="portfolio-container">
+                            <Fade bottom>
+                                <div>
+                                    <img src={kroontje}/>
+                                    <h2>Eethuys 't Kroontje</h2>
+                                    {/*<p>The website for Eethuys 't Kroontje was one of the first websites we actually built. It was made using a very simple and responsive design. If you're looking for good food in a relaxing environment, check out their site by clicking the image above. If you're curious to a super secret prototype we made, click this.</p>*/}
+                                </div>
 
-                        <div>
-                            <img src={kroontje}/>
-                            <h2>Eethuys 't Kroontje</h2>
-                        </div>
+                                <div>
+                                    <img src={kroontje}/>
+                                    <h2>Eethuys 't Kroontje</h2>
+                                </div>
 
-                        <div>
-                            <img src={kroontje}/>
-                            <h2>Eethuys 't Kroontje</h2>
+                                <div>
+                                    <img src={kroontje}/>
+                                    <h2>Eethuys 't Kroontje</h2>
+                                </div>
+                            </Fade>
                         </div>
                     </div>
-                </div>
+                </Fade>
 
-                <h1>test</h1>
+                <h1>Desktop</h1>
             </div>
         );
     }
