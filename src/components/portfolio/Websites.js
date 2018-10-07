cdcffcd3723a1bf82a84d19f9ed569aa3d68212b
Amar Ramdas <amarramdas97@gmail.com>
2018-10-07 21:49:47 +0200

    Add beginnings for Portfolio page

--- src/components/portfolio/Websites.js
+++ src/components/portfolio/Websites.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
+import kroontje from '../../images/portfolio/kroontje.png'
 
 class Websites extends Component {
 	render() {
 		return (
 			<div className="websites">
+				<h1>Web</h1>
+				<div className="portfolio-container">
+					<div>
+						<img src={kroontje}/>
+						<h2>Eethuys 't Kroontje</h2>
+					</div>
 
+                    <div>
+                        <img src={kroontje}/>
+                        <h2>Eethuys 't Kroontje</h2>
+                    </div>
+
+                    <div>
+                        <img src={kroontje}/>
+                        <h2>Eethuys 't Kroontje</h2>
+                    </div>
+				</div>
+
+				<h1>test</h1>
 			</div>
 		);
 	}
