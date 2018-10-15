5b29655666d4cbb4073ff61cc7f2a30cffb858e7
Amar97 <amarramdas97@gmail.com>
2018-10-15 17:52:40 +0200

    Update text for service summary and clean up code
    Signed-off-by: Amar97 <amarramdas97@gmail.com>

--- src/components/home/ServiceSummary.js
+++ src/components/home/ServiceSummary.js
@@ -7,72 +7,61 @@ import {translate} from "react-multi-lang"
 import Cards from "../Cards";
 
 const why = [
-    {
-        logo: "",
-        service: "multi",
-    },
-    {
-        logo: "",
-        service: "through",
-    },
+	{
+		logo: "",
+		service: "multi",
+	},
+	{
+		logo: "",
+		service: "transparency",
+	},
 	{
 		service: "integrations"
-	}
+	},
 ];
 
 
 class ServiceSummary extends Component {
-    render() {
-        return (
-            <div className="services-wrapper">
-                <h1>Why ASRR?</h1>
-                <p>Why choose ASRR instead of a conventional freelancer (ZZP) or an internal IT employee?</p>
-	            <Cards category="why" items={why}/>
-                <ul>
-                    <li><b>Multidisciplinary team</b>
-                        <p>While one developer might be able to get something outside of the scope of their expertise to
-                            work, it quickly becomes messy with larger and more complex projects. This is where our
-                            service makes a difference, by combining the diverse knowledge of our developers to tailor
-                            to the customers needs, using the best practices and state of the art techniques.</p>
+	render() {
+		return (
+			<div className="services-wrapper">
+				<div>
+					<h1>Why ASRR?</h1>
+					<p>Why choose ASRR instead of a conventional freelancer (ZZP) or an internal IT employee?</p>
+					<Cards category="why" items={why}/>
+				</div>
 
-                    </li>
-                    <li>Large throughput</li>
-                    <li>Quick</li>
-                    <li>Price</li>
-                </ul>
+				<hr/>
+				<div>
+					<h1> What we do</h1>
 
+					<div className="flex-container">
+						<div>
+							<img src={laptop} alt="webdesign"/>
+							<h2>Webdesign</h2>
+							<p>{this.props.t('home.web')}</p>
+						</div>
+						<div>
+							<img src={phone} alt="app development"/>
+							<h2>App Development</h2>
+							<p>{this.props.t('home.app')}</p>
+						</div>
+						<div>
+							<img src={router} alt="back-ends"/>
+							<h2>Custom back-end applications</h2>
+							<p>{this.props.t('home.backend')}</p>
+						</div>
+						<div>
+							<img src={computer} alt="desktop"/>
+							<h2>Windows Desktop Applications</h2>
+							<p>{this.props.t('home.desktop')}</p>
+						</div>
+					</div>
+				</div>
 
-                <hr/>
-
-                <div>
-                    <h1> What we do</h1>
-                </div>
-
-                <div className="flex-container">
-                    <div>
-                        <img src={laptop} alt="webdesign"/>
-                        <h2>Webdesign</h2>
-                        <p>{this.props.t('home.web')}</p>
-                    </div>
-                    <div>
-                        <img src={phone} alt="app development"/>
-                        <h2>App Development</h2>
-                        <p>{this.props.t('home.app')}</p>
-                    </div>
-                    <div>
-                        <img src={router} alt="back-ends"/>
-                        <h2>Custom back-end applications</h2>
-                        <p>{this.props.t('home.backend')}</p>
-                    </div>
-                    <div>
-                        <img src={computer} alt="desktop"/>
-                        <h2>Windows Desktop Applications</h2>
-                        <p>{this.props.t('home.desktop')}</p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+			</div>
+		);
+	}
 }
 
 export default translate(ServiceSummary);
