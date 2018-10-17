da46f756f03be0505447fb002f0e0fd2ebe8083b
Amar Ramdas <amarramdas97@gmail.com>
2018-10-17 10:29:07 +0200

    Add Achievements and business case for Form
    Signed-off-by: Amar Ramdas <amarramdas97@gmail.com>

---
+++ src/components/portfolio/Achievements.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Fade from 'react-reveal/Fade';
+
+class Achievements extends Component {
+    render() {
+        return (
+            <div className="achievements">
+                <Fade bottom>
+                    {/*TODO: Make data smart*/}
+                    <h1>What we've done so far</h1>
+                    <div className="row-container">
+                        <div>
+                            <h2>9 happy customers</h2>
+                        </div>
+                        <div>
+                            <h2>10+ commercial websites, 2 full stack web shops, 2 side projects</h2>
+                        </div>
+                        <div>
+                            <h2>2 ASRR Servers</h2>
+                        </div>
+                    </div>
+                </Fade>
+            </div>
+        );
+    }
+}
+
+export default Achievements;
---
+++ src/components/portfolio/BusinessCases.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import Fade from 'react-reveal/Fade';
+
+class BusinessCases extends Component {
+    render() {
+        return (
+            <div className="businesscases">
+                <Fade bottom>
+                    <h1>Business Cases</h1>
+                    <hr/>
+                    <div className="row-container">
+                        <div>
+                            <img/>
+                        </div>
+                        <div>
+                            <h2>FORM Architectuur & Engineering</h2>
+                            <p>
+                                For FORM, we set up a windows server. As most of our client data is confidential, we
+                                can't say much about the contents of this server. What we can tell is what we've done.
+                                First off, we set up the server from scratch, providing remote desktop capabilities,
+                                opening up the correct ports for API access and installing the required programs.
+                            </p>
+                            <p>
+                                Next, we set up one of our continuous deployment systems for their internal product.
+                                This system automatically closes the program using the code and downloads the latest
+                                version whenever a new stable build is available on their private GitHub repository.
+                                The project is then built locally in the required folder and is automatically signed.
+                                After all tests have passed, the back-end spins up again and is ready to handle requests
+                                again.
+                            </p>
+                            <h3>Features</h3>
+                            <ul>
+                                <li>Server Setup</li>
+                                <li>Continuous Deployment</li>
+                                <li>Simple Back-End</li>
+                            </ul>
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                </Fade>
+            </div>
+        );
+    }
+}
+
+export default BusinessCases;
