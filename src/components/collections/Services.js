378e2f004c9957c8bbbf9758065f43514e682392
Amar Ramdas <amarramdas97@gmail.com>
2018-12-15 15:13:31 +0100

    Add correct images for services
    Signed-off-by: Amar Ramdas <amarramdas97@gmail.com>

--- src/components/collections/Services.js
+++ src/components/collections/Services.js
@@ -5,31 +5,47 @@ import purple from "../../images/backgrounds/purple.svg"
 import server from "../../images/sectionimages/work.png"
 import ServiceGrid from "../services/ServiceGrid";
 
+import web from "../../images/services/webdesign.svg"
+import app from "../../images/services/app.svg"
+import desktop from "../../images/services/responsive.svg"
+import backend from "../../images/services/back-end.svg"
+import database from "../../images/services/database.svg"
+import data from "../../images/services/data.svg"
+import bim from "../../images/services/bim.svg"
+import consultancy from "../../images/services/consultancy.svg"
+
+
 const services = [
 	{
-		logo: "",
+		image: web,
 		service: "web",
 	},
 	{
-		logo: "",
+		image: app,
 		service: "app",
 	},
 	{
-		service: "desktop"
+        image: desktop,
+        service: "desktop"
 	},
 	{
-		service: "backend"
+        image: backend,
+        service: "backend"
 	},
 	{
+		image: database,
 		service: "database"
 	},
 	{
+		image: data,
 		service: "data-analysis"
 	},
 	{
+		image: bim,
 		service: "bim"
 	},
 	{
+		image: consultancy,
 		service: "consultancy"
 	}
 ];
--- src/components/services/ServiceGrid.js
+++ src/components/services/ServiceGrid.js
@@ -17,6 +17,7 @@ class ServiceGrid extends Component {
                             <Fade bottom>
 	                            {this.props.items.map((item, index) => {
 		                            return <div key={index} className="drop-shadow">
+                                        <img src={item.image} class="service-image" alt={item.service + " image"}/>
                                         <h2>{this.props.t('services.' + item.service + ".name")}</h2>
                                         <div className="card-description">
                                             <p><ReadMoreReact text={this.props.t('services.' + item.service + ".description")} min={30} ideal={35} max={40} /></p>
