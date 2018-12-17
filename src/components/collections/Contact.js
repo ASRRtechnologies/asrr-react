4435804d310c7a5c5147c6624ddc1d924be26453
Amar97 <amarramdas97@gmail.com>
2018-12-17 01:28:44 +0100

    Extend contact page
    Signed-off-by: Amar97 <amarramdas97@gmail.com>

--- src/components/collections/Contact.js
+++ src/components/collections/Contact.js
@@ -3,14 +3,32 @@ import Fade from "react-reveal/Fade";
 import Landing from "../Landing";
 import green from "../../images/backgrounds/green.svg";
 import contact from "../../images/sectionimages/contact.svg";
+import ContactMethods from "../contact/ContactMethods";
+import mail from "../../images/contact/mail.svg";
+import phone from "../../images/contact/mobile.svg";
+import social from "../../images/contact/social.svg";
 
-const button =
+// const button =
+// 	{
+// 		text: "buttons.contact",
+// 		href: "mailto:contact@asrr.nl",
+// 		fa: ["fas", "envelope"]
+// 	};
+
+const services = [
 	{
-		text: "buttons.contact",
-		href: "mailto:contact@asrr.nl",
-		fa: ["fas", "envelope"]
-	};
-
+		image: mail,
+		service: "mail",
+	},
+	{
+		image: phone,
+		service: "phone",
+	},
+	{
+		image: social,
+		service: "social"
+	}
+];
 
 class Contact extends Component {
     render() {
@@ -18,7 +36,8 @@ class Contact extends Component {
             <div>
                 <Fade bottom>
 	                <Landing header="contact.headline" subtitle="contact.subtitle" background={green} image={contact}
-	                         button={button}/>
+	                />
+	                <ContactMethods items={services}/>
                 </Fade>
             </div>
         );
---
+++ src/components/contact/ContactMethods.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import Fade from 'react-reveal/Fade';
+import {translate} from 'react-multi-lang';
+import ReadMoreReact from "read-more-react";
+
+// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+
+class ContactMethods extends Component {
+	render() {
+		return (
+			<div className="grid-container white">
+				<Fade bottom>
+					<div className="item-container">
+						<div className="item-cards">
+							<Fade bottom>
+								{this.props.items.map((item, index) => {
+									return <div key={index} className="drop-shadow">
+										<img src={item.image} className="service-image" alt={item.service + " image"}/>
+										<h2>{this.props.t('contact.' + item.service + ".name")}</h2>
+										<div className="card-description">
+											<div><ReadMoreReact
+												text={this.props.t('contact.' + item.service + ".description")}
+												min={30} ideal={32} max={35}/></div>
+										</div>
+									</div>
+								})}
+							</Fade>
+						</div>
+					</div>
+				</Fade>
+
+			</div>
+		);
+	}
+}
+
+export default translate(ContactMethods);
