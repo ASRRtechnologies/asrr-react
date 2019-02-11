46c8b360b74f0fce977c469f3f58dd71a43d1395
Amar97 <amarramdas97@gmail.com>
2019-02-11 23:58:59 +0100

    Clean up service summary
    Signed-off-by: Amar97 <amarramdas97@gmail.com>

--- src/components/home/ServiceSummary.js
+++ src/components/home/ServiceSummary.js
@@ -1,12 +1,9 @@
 import React, {Component} from "react";
 import laptop from "../../images/services/laptop.svg";
 import router from "../../images/services/router.svg";
-import computer from "../../images/services/computer.svg";
 import phone from "../../images/services/phone.svg";
 import {translate} from "react-multi-lang"
 import {Link} from "react-router-dom";
-
-import ReadMoreReact from 'read-more-react';
 
 class ServiceSummary extends Component {
 	render() {
@@ -28,25 +25,19 @@ class ServiceSummary extends Component {
 						<div>
 							<img src={laptop} alt="webdesign"/>
 							<h2>{this.props.t('home.services.web.name')}</h2>
-                            <div><ReadMoreReact text={this.props.t('home.services.web.description')}/></div>
+							<div>{this.props.t('home.services.web.description')}</div>
 						</div>
 
                         <div>
 							<img src={phone} alt="app development"/>
 							<h2>{this.props.t('home.services.app.name')}</h2>
-                            <div><ReadMoreReact text={this.props.t('home.services.app.description')}/></div>
+	                        <div>{this.props.t('home.services.app.description')}</div>
 						</div>
 
                         <div>
 							<img src={router} alt="back-ends"/>
 							<h2>{this.props.t('home.services.backend.name')}</h2>
-                            <div><ReadMoreReact text={this.props.t('home.services.backend.description')}/></div>
-
-						</div>
-						<div>
-							<img src={computer} alt="desktop"/>
-							<h2>{this.props.t('home.services.desktop.name')}</h2>
-                            <div><ReadMoreReact text={this.props.t('home.services.desktop.description')}/></div>
+	                        <div>{this.props.t('home.services.backend.description')}</div>
 
 						</div>
 					</div>
