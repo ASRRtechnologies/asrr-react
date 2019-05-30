163a5ec55de8d8ab532b8da78dd3e64aa7bfcba3
Amar Ramdas <amarramdas97@gmail.com>
2019-05-30 16:38:34 +0200

    Update landing text

--- src/components/shared/landing/LandingText.js
+++ src/components/shared/landing/LandingText.js
@@ -16,6 +16,17 @@ class LandingText extends Component {
 						<img src={laptop}/>
 						<div className="fluid-landing-image-text">
 							<h1>What do we do?</h1>
+							<div className="fluid-landing-flex-grid">
+								<div className="fluid-landing-col">
+									<h2>Develop custom tailored software</h2>
+								</div>
+								<div className="fluid-landing-col">
+									<h2>Build and host blazing fast servers/desktops</h2>
+								</div>
+								<div className="fluid-landing-col">
+									<h2>Provide flexible developers</h2>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
