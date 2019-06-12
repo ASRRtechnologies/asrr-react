7c4ad285a2891a20b2d95ec141f2cbfcc8b319f1
hesusdios <Logitech11>
2019-06-13 01:20:51 +0200

    Better responsive card

--- src/components/work/WorkCard.js
+++ src/components/work/WorkCard.js
@@ -4,7 +4,16 @@ class WorkCard extends Component {
     render() {
         const {title, brand} = this.props;
         return (
-            <div className="work-card-wrapper">
+            <div className="card">
+                <div style={{backgroundImage: "url(https://picsum.photos/500/300/?image=10)"}}  className="card_image"> </div>
+                <div className="card_content">
+                    <h2 className="card_title">Card Grid Layout</h2>
+                    <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
+                        layout</p>
+                    <button className="btn card_btn">Read More</button>
+                </div>
+            </div> );
+            {/*<div className="work-card-wrapper">*/}
                 {/*<div className="work-card">*/}
                     {/*<div className="work-card-image">*/}
                         {/*<h2> Text</h2>*/}
@@ -30,24 +39,13 @@ class WorkCard extends Component {
                 {/*</div>*/}
 
                 {/*</div>*/}
-                <div className="cards">
-                    <li className="cards_item">
-                        <div className="card">
-                            <div style={{backgroundImage: "url(https://picsum.photos/500/300/?image=10)"}}  className="card_image"> </div>
-                            <div className="card_content">
-                                <h2 className="card_title">Card Grid Layout</h2>
-                                <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
-                                    layout</p>
-                                <button className="btn card_btn">Read More</button>
-                            </div>
-                        </div>
-                    </li>
-                </div>
+                {/*<div className="cards">*/}
+                    {/*<li className="cards_item">*/}
 
+                    {/*</li>*/}
+                {/*</div>*/}
+            {/*</div>*/}
 
-
-            </div>
-        );
     }
 }
 
