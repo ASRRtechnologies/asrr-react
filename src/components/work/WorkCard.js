b3b0ff68c0337dd3ae64d670283183609cd6d24d
hesusdios <Logitech11>
2019-06-12 21:18:58 +0200

    Create responsive work-card

--- src/components/work/WorkCard.js
+++ src/components/work/WorkCard.js
@@ -5,16 +5,45 @@ class WorkCard extends Component {
         const {title, brand} = this.props;
         return (
             <div className="work-card-wrapper">
-                <div className="work-card-image">
+                {/*<div className="work-card">*/}
+                    {/*<div className="work-card-image">*/}
+                        {/*<h2> Text</h2>*/}
+                    {/*</div>*/}
 
-                </div>
+                    {/*<div className="work-card-description">*/}
+                        {/*<p>IT Beheer</p>*/}
+                        {/*<h2>FORM</h2>*/}
+                        {/*<p className="work-card-read-more">Read more</p>*/}
+                    {/*</div>*/}
+                {/*</div>*/}
+                {/*<div className="work-card-wrapper">*/}
+                {/*<div className="work-card">*/}
+                {/*<div className="work-card-image">*/}
+                {/*<h2> Text</h2>*/}
+                {/*</div>*/}
 
-                <div className="work-card-description">
-                    <p>IT Beheer</p>
-                    <h2>FORM</h2>
-                    <p className="work-card-read-more">Read more</p>
+                {/*<div className="work-card-description">*/}
+                {/*<p>IT Beheer</p>*/}
+                {/*<h2>FORM</h2>*/}
+                {/*<p className="work-card-read-more">Read more</p>*/}
+                {/*</div>*/}
+                {/*</div>*/}
 
-                </div>
+                {/*</div>*/}
+                    <li className="cards_item">
+                        <div className="card">
+                            <div className="card_image"><img src="https://picsum.photos/500/300/?image=10"/></div>
+                            <div className="card_content">
+                                <h2 className="card_title">Card Grid Layout</h2>
+                                <p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
+                                    layout</p>
+                                <button className="btn card_btn">Read More</button>
+                            </div>
+                        </div>
+                    </li>
+
+
+
             </div>
         );
     }
--- src/components/work/WorkGrid.js
+++ src/components/work/WorkGrid.js
@@ -4,11 +4,76 @@ import WorkCard from "./WorkCard";
 class WorkGrid extends Component {
 	render() {
 		return (
-			<div className="work-grid-wrapper">
-				<WorkCard/>
-				<WorkCard/>
-				<WorkCard/>
-
+			<div className="main">
+				<h1>Responsive Card Grid Layout</h1>
+				<ul className="cards">
+					<li className="cards_item">
+						<div className="card">
+							<div className="card_image"><img src="https://picsum.photos/500/300/?image=10"/></div>
+							<div className="card_content">
+								<h2 className="card_title">Card Grid Layout</h2>
+								<p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
+									layout</p>
+								<button className="btn card_btn">Read More</button>
+							</div>
+						</div>
+					</li>
+					<li className="cards_item">
+						<div className="card">
+							<div className="card_image"><img src="https://picsum.photos/500/300/?image=5"/></div>
+							<div className="card_content">
+								<h2 className="card_title">Card Grid Layout</h2>
+								<p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
+									layout</p>
+								<button className="btn card_btn">Read More</button>
+							</div>
+						</div>
+					</li>
+					<li className="cards_item">
+						<div className="card">
+							<div className="card_image"><img src="https://picsum.photos/500/300/?image=11"/></div>
+							<div className="card_content">
+								<h2 className="card_title">Card Grid Layout</h2>
+								<p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
+									layout</p>
+								<button className="btn card_btn">Read More</button>
+							</div>
+						</div>
+					</li>
+					<li className="cards_item">
+						<div className="card">
+							<div className="card_image"><img src="https://picsum.photos/500/300/?image=14"/></div>
+							<div className="card_content">
+								<h2 className="card_title">Card Grid Layout</h2>
+								<p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
+									layout</p>
+								<button className="btn card_btn">Read More</button>
+							</div>
+						</div>
+					</li>
+					<li className="cards_item">
+						<div className="card">
+							<div className="card_image"><img src="https://picsum.photos/500/300/?image=17"/></div>
+							<div className="card_content">
+								<h2 className="card_title">Card Grid Layout</h2>
+								<p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
+									layout</p>
+								<button className="btn card_btn">Read More</button>
+							</div>
+						</div>
+					</li>
+					<li className="cards_item">
+						<div className="card">
+							<div className="card_image"><img src="https://picsum.photos/500/300/?image=2"/></div>
+							<div className="card_content">
+								<h2 className="card_title">Card Grid Layout</h2>
+								<p className="card_text">Demo of pixel perfect pure CSS simple responsive card grid
+									layout</p>
+								<button className="btn card_btn">Read More</button>
+							</div>
+						</div>
+					</li>
+				</ul>
 			</div>
 		);
 	}
