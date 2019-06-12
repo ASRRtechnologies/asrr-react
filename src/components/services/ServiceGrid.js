e1b96c7a8e976d9d9ab891cc9d51965909a19d5d
Amar Ramdas <amarramdas97@gmail.com>
2019-06-12 21:50:28 +0200

    Add cards for services

--- src/components/services/ServiceGrid.js
+++ src/components/services/ServiceGrid.js
@@ -4,36 +4,28 @@ import {translate} from 'react-multi-lang';
 import ReadMoreReact from "read-more-react";
 
 // import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-
-
 class ServiceGrid extends Component {
-    render() {
-        return (
-            <div className="grid-container white">
-                <h1>{this.props.t(this.props.section)}</h1>
-                <Fade bottom>
-                    <div className="item-container">
-                        <div className="item-cards">
-                            <Fade bottom>
-	                            {this.props.items.map((item, index) => {
-		                            return <div key={index} className="drop-shadow">
-			                            <img src={item.image} className="service-image" alt={item.service + " image"}/>
-                                        <h2>{this.props.t('services.' + item.service + ".name")}</h2>
-                                        <div className="card-description">
-                                            <div><ReadMoreReact
-                                                text={this.props.t('services.' + item.service + ".description")}
-                                                min={30} ideal={32} max={35}/></div>
-                                        </div>
-                                    </div>
-                                })}
-                            </Fade>
-                        </div>
-                    </div>
-                </Fade>
-
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="service-grid-container white">
+				<div className="service-grid-items-container">
+					<Fade bottom>
+						{this.props.items.map((item, index) => {
+							return <div key={index} className="drop-shadow">
+								<img src={item.image} className="service-image" alt={item.service + " image"}/>
+								<h2>{this.props.t('services.' + item.service + ".name")}</h2>
+								<div className="card-description">
+									<div><ReadMoreReact
+										text={this.props.t('services.' + item.service + ".description")}
+										min={30} ideal={32} max={35}/></div>
+								</div>
+							</div>
+						})}
+					</Fade>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default translate(ServiceGrid);
