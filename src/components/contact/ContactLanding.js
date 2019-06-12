859cb3c940c222f96785487c72b193f1b25a50bf
Amar Ramdas <amarramdas97@gmail.com>
2019-06-12 20:56:36 +0200

    Update contact landing and remove unused code

--- src/components/contact/ContactLanding.js
+++ src/components/contact/ContactLanding.js
@@ -4,7 +4,9 @@ class ContactLanding extends Component {
 	render() {
 		return (
 			<div className="contact-landing-wrapper">
-				test
+				<div className="contact-landing-wrapper">
+					<h1>Contact us</h1>K
+				</div>
 			</div>
 		);
 	}
