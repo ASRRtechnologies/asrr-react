5d29288e6460d7200af59cac1e499163c3f36585
Re-Angelo Jarvis <hesusdios@gmail.com>
2019-06-15 16:24:16 +0200

    Creates hoverable-overlay image

--- src/components/work/WorkCard.js
+++ src/components/work/WorkCard.js
@@ -1,20 +1,26 @@
 import React, {Component} from 'react';
 import Arrow from "../../assets/images/work/icons/angle-arrow-pointing-to-right.svg";
+import Plant from "../../assets/images/work/plants.jpg";
 
 class WorkCard extends Component {
     render() {
-        const {title, client, logo, alt} = this.props;
+        const {title, client, logo, alt, background, backgroundOverlay} = this.props;
         return (
-            <div className="card">
-                <div style={{backgroundImage: "url(https://picsum.photos/500/300/?image=10)"}} className="card_image">
-                    <img className="card-logo" alt={alt} src={logo}/>
+            <div className="work-card">
+
+                <div style={{backgroundImage: `url(${background})`}} className="work-card_image">
+                    <div style={{backgroundImage: `url(${backgroundOverlay})`}}
+                         className="work-card-overlay">
+                    </div>
+                    <img className="work-card-logo" alt={alt} src={logo}/>
                 </div>
-                <div className="card_content">
-                    <p className="card-title">{title}</p>
-                    <h2 className="card_client">{client}</h2>
-                    <div className="card-read-more">
+
+                <div className="work-card_content">
+                    <p className="work-card-title">{title}</p>
+                    <h2 className="work-card_client">{client}</h2>
+                    <div className="work-card-read-more">
                         <p>Read More</p>
-                        <img src={Arrow} alt="arrow" className="card-read-more-arrow"/>
+                        <img src={Arrow} alt="arrow" className="work-card-read-more-arrow"/>
                     </div>
                 </div>
             </div>
