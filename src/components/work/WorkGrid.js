d444cbf2f4ab456fecc51ebf348d46ce9013509e
Re-Angelo Jarvis <hesusdios@gmail.com>
2019-06-15 17:30:09 +0200

    Make background image scale down

--- src/components/work/WorkGrid.js
+++ src/components/work/WorkGrid.js
@@ -5,13 +5,15 @@ import Google from "../../assets/images/work/google.png"
 import Amazon from "../../assets/images/work/amazon.png"
 import Plant from "../../assets/images/work/plants.jpg"
 import PlantOverlay from "../../assets/images/work/plants-overlay.png"
+import Building from "../../assets/images/work/building.jpg";
+import BuildingOverlay from "../../assets/images/work/building-overlay.png"
 
 class WorkGrid extends Component {
 	render() {
 		return (
 			<div className="work-grid-wrapper">
 				<div className="work-card-wrapper">
-					<WorkCard logo={Nike} background={Plant} backgroundOverlay={PlantOverlay}  alt="Nike-logo" client="Nike" title="Product design"/>
+					<WorkCard logo={Nike} background={Building} backgroundOverlay={BuildingOverlay}  alt="Nike-logo" client="Nike" title="Product design"/>
 					<WorkCard logo={Google} background={Plant} backgroundOverlay={PlantOverlay} alt="Google-logo" client=" Google" title="Website design"/>
 					<WorkCard logo={Amazon} background={Plant} backgroundOverlay={PlantOverlay} alt="Amazon-logo" client="Amazon" title="Consultancy"/>
 				</div>
