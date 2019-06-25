66480f7fd52c5686e2890eeaf73304c6feb1cf62
hesusdios <Logitech11>
2019-06-26 01:50:55 +0200

    Use diffeent path for from logo

--- src/components/work/WorkGrid.js
+++ src/components/work/WorkGrid.js
@@ -8,14 +8,13 @@ import PlantOverlay from "../../assets/images/work/plants-overlay.png";
 import Building from "../../assets/images/work/form/building-min.png";
 import BuildingOverlay from "../../assets/images/work/form/building-overlay-min.png";
 import FormLogo from  "../../assets/images/work/form/form_logo.svg";
-import form from "../../assets/images/clients/form_logo-diap.svg";
 
 class WorkGrid extends Component {
 	render() {
 		return (
 			<div className="work-grid-wrapper">
 				<div className="work-card-wrapper">
-					<WorkCard logo={form} background={Building} backgroundOverlay={BuildingOverlay}  alt="Form-logo" client="Form Architecture" title="Product design"/>
+					<WorkCard logo={FormLogo} background={Building} backgroundOverlay={BuildingOverlay}  alt="Form-logo" client="Form Architecture" title="Product design"/>
 					<WorkCard logo={Google} background={Plant} backgroundOverlay={PlantOverlay} alt="Google-logo" client=" Google" title="Website design"/>
 					<WorkCard logo={Amazon} background={Plant} backgroundOverlay={PlantOverlay} alt="Amazon-logo" client="Amazon" title="Consultancy"/>
 				</div>
