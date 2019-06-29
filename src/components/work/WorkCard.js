dcf8880e622b0b68ebdc562089872cb71a650c54
hesusdios <Logitech11>
2019-06-29 18:37:16 +0200

    Add filter div to create dark overlay on image and exclude client logo

--- src/components/work/WorkCard.js
+++ src/components/work/WorkCard.js
@@ -7,18 +7,16 @@ class WorkCard extends Component {
         return (
             <div className="work-card">
                 <div style={{backgroundImage: `url(${background})`}} className="work-card_image">
-                    <div className="work-card-filter">
-                    <div style={{backgroundImage: `url(${backgroundOverlay})`}}
-                             className="work-card-overlay">
+                    <div style={{backgroundImage: `url(${backgroundOverlay})`}} className="work-card-overlay">
                     </div>
-                    </div>
+                    <div className="work-card-dark-filter"> </div>
                     <img className="work-card-logo" alt={alt} src={logo}/>
                 </div>
                 <div className="work-card_content">
                     <p className="work-card-title">{title}</p>
                     <h2 className="work-card_client">{client}</h2>
                     <div className="work-card-read-more">
-                        <span style={{display:"inline"}} >
+                        <span style={{display: "inline"}}>
                             <p>Read More</p>
                             <img src={Arrow} alt="arrow" className="work-card-read-more-arrow"/>
                         </span>
