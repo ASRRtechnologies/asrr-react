c4124d74120e86fc35c3ebb637014da706e2713e
Re-Angelo Jarvis <hesusdios@gmail.com>
2019-09-10 12:19:35 +0200

    Add AOS to elements

--- src/components/home/Section.js
+++ src/components/home/Section.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import NWO from "../../assets/images/home/laptop-nwo.png";
+import AOS from 'aos';
 
 class Section extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  componentDidMount () {
+	    AOS.init()
+	}
+
+
   render() {
+
     return (
       <div className="section-container">
-        <div className="section-text">
-          <h2>Remote desktop server</h2>
-          <p>
+        <div  className="section-text">
+          <h2 data-aos="fade-right" data-aos-easing="ease-in-out"
+							 data-aos-anchor-placement="top"
+							 data-aos-duration="600">Remote desktop server</h2>
+        
+        
+          <p data-aos="fade-right" data-aos-delay="200" data-aos-easing="ease-in-out"
+							 data-aos-anchor-placement="top"
+							 data-aos-duration="600">
             Lorem Ipsum is slechts een proeftekst uit het drukkerij- en
             zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze
             bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een
@@ -20,15 +34,21 @@ class Section extends Component {
             maar is ook, vrijwel onveranderd, overgenomen in elektronische
             letterzetting. Het is in de jaren '60 populair geworden met de{" "}
           </p>
-          <span>See all projects</span>
+          <span data-aos="fade-right" data-aos-delay="400" data-aos-easing="ease-in-out"
+							 data-aos-anchor-placement="top"
+							 data-aos-duration="600">See all projects</span>
           
-          <div class="section-button">
+          <div data-aos="fade-up" data-aos-delay="600" data-aos-easing="ease-in-out"
+							 data-aos-anchor-placement="top"
+							 data-aos-duration="600" class="section-button">
             <p>Read more</p>
           </div>
           
         </div>
 
-        <div className="section-image">
+        <div  data-aos="fade-up" data-aos-delay="400" data-aos-easing="ease-in-out"
+							 data-aos-anchor-placement="top"
+							 data-aos-duration="600" className="section-image">
           <img id="nwo-laptop-image" src={NWO} />
 
           {/* <div id="nwo-laptop-image-wrapper"> 
